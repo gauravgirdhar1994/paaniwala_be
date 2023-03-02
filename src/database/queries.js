22d1fbe33f8f38d585430f743d3cb4e273f8242a
Gaurav Girdhar <gauravgirdhar1994@gmail.com>
2023-03-02 10:55:18 +0530

    update signup api

--- src/database/queries.js
+++ src/database/queries.js
@@ -11,12 +11,13 @@ CREATE TABLE IF NOT EXISTS users (
     lastname VARCHAR(50) NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
     created_on TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 )
 `;
 
 const createNewUser = `
-INSERT INTO users VALUES(null, ?, ?, ?, ?, NOW())
+INSERT INTO users (id, firstname, lastname, email, password, role, created_on) VALUES(null, ?, ?, ?, ?, ?, NOW())
 `;
 
 const findUserByEmail = `
