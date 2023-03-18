9d97f1a609d2285da788890f4572bfc04af2af49
Gaurav Girdhar <gauravgirdhar1994@gmail.com>
2023-03-19 04:06:58 +0530

    serve images from public

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(morgan('combined', { stream: httpLogStream }));
 app.use(cors());
